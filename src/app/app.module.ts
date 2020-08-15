5071a8bb779505bbe6b5125bd8b493234da9f34d
Eli <elionlink@gmail.com>
2020-08-15 19:51:37 +0300

    150820

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { NgModule } from '@angular/core';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { MatInputModule } from '@angular/material/input';
 
@@ -37,5 +37,6 @@ import { TodosAddComponent } from './todos.add/todos.add.component';
   ],
   providers: [ApiDataSource, DataRepository],
   bootstrap: [AppComponent],
+  schemas: [NO_ERRORS_SCHEMA],
 })
 export class AppModule {}
